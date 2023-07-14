40de168894772bda2720d886a50d1ccd2d09f704
its-kayes <kayes.ek8@gmail.com>
2023-07-14 17:03:03 +0600

    Implement header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,13 @@
+import Header from '../Components/Header';
+import { Outlet } from 'react-router-dom';
+
 export default function MainLayout() {
-  return <div> MainLayout </div>;
+  return (
+    <div>
+      <Header />
+      <div className="pt-6">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -8,32 +8,45 @@ import Home from '../pages/Home';
 import AddNewBook from '../pages/AddNewBook';
 import BookDetails from '../pages/BookDetails';
 import EditBook from '../pages/EditBook';
+import App from '../App';
 
 const routes = createBrowserRouter([
   {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
     path: '/',
-    element: <Home />,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/add-new-book',
+        element: <AddNewBook />,
+      },
+      {
+        path: '/book-details/:id',
+        element: <BookDetails />,
+      },
+      {
+        path: '/edit-book',
+        element: <EditBook />,
+      },
+      {
+        path: '/signin',
+        element: <Signin />,
+      },
+      {
+        path: '/signup',
+        element: <Signup />,
+      },
+    ],
   },
-  {
-    path: '/add-new-book',
-    element: <AddNewBook />,
-  },
-  {
-    path: '/book-details/:id',
-    element: <BookDetails />,
-  },
-  {
-    path: '/edit-book',
-    element: <EditBook />,
-  },
-  {
-    path: '/signin',
-    element: <Signin />,
-  },
+
+  // {
+  //   path: '/',
+  //   element: <Home />,
+  // },
+
   {
     path: '*',
     element: <NotFound />,
